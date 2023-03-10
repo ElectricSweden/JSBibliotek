1de30c2a6bf08f2aedcba78aed460cf6aeff2c79
ElectricSweden <olle.nagard@gmail.com>
2023-03-10 19:44:19 +0100

    styling library + fixing covers and functionality

--- script.js
+++ script.js
@@ -2,18 +2,23 @@ let books = document.getElementById("books");
 const borrowBtn = document.getElementsByClassName("borrow");
 const genLib = document.getElementById("generateLibrary").addEventListener("click", drawBooks);
 
+
 function showAvailable(book, count)
 {
     return `
     <div class="book" id="${book.ISBN}">
-        <p>${book.title}</p>
-        <ul>
-            <li>Title: ${book.title}</li>
-            <li>Author: ${book.author}</li>
-            <li>ISBN: ${book.ISBN}</li>
-        </ul>
+        <p>Title: ${book.title}</p>
+        <p>Author: ${book.author}</p>
+        <p>ISBN: ${book.ISBN}</p>
         <button class="borrow" onclick="borrow(${book.ISBN})">Borrow</button>
     </div>
+    `;
+}
+
+function showCover(book, count)
+{
+    return `
+    <img src="${book.cover}"  onclick="drawContent(${book.ISBN})">
     `;
 }
 
@@ -21,14 +26,14 @@ const book = {
     title: "undefined",
     author: "undefined",
     ISBN: 0,
-    availability: true
-    //cover:
+    availability: true,
+    cover: "undefined"
 }
 
 var library = [
-    { title: "Harry Potter and the Sorcerer’s Stone", author: "JK.Rowling", ISBN: 1, availability: true},
-    { title: "Harry Potter and the Chamber of Secrets", author: "JK.Rowling", ISBN: 2, availability: true},
-    { title: "Harry Potter and the Prisoner of Azkaban", author: "JK.Rowling", ISBN: 3, availability: true}
+    { title: "Harry Potter and the Philosopher's Stone", author: "JK.Rowling", ISBN: 1, availability: true, cover: "./covers/harrypotterstonecover.jpg"},
+    { title: "Harry Potter and the Chamber of Secrets", author: "JK.Rowling", ISBN: 2, availability: true, cover: "./covers/chamber2.jpg"},
+    { title: "Harry Potter and the Prisoner of Azkaban", author: "JK.Rowling", ISBN: 3, availability: true, cover: "./covers/harrypotterprisoner.jpg"}
 ];
 
 
@@ -40,7 +45,7 @@ function borrow(bookISBN)
             if (book.ISBN == bookISBN)
                 book.availability = false;
         });
-        
+        document.getElementById("title").style.display = "flex";
         console.log(bookISBN + " borrowed successfully.");
         drawBooks();
     }
@@ -48,10 +53,31 @@ function borrow(bookISBN)
 
 function drawBooks()
 {
+    var anyAvailable = 0;
     var count = 0;
     books.innerHTML = "";
     library.forEach(book => {
         if (book.availability == true)
+        {
+            books.innerHTML += showCover(book, count)
+            count++
+            anyAvailable = 1;
+        }
+    })
+    if (anyAvailable == 0)
+    {
+        document.getElementById("title").innerHTML = "No available books";
+    }
+}
+
+
+function drawContent(bookISBN)
+{
+    var count = 0;
+    books.innerHTML = "";
+    document.getElementById("title").style.display = "none";
+    library.forEach(book => {
+        if (book.ISBN == bookISBN)
         {
             books.innerHTML += showAvailable(book, count)
             count++
